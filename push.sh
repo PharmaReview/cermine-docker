e2e87010a1b652bc5c1e351c54331ff5455296c4
Simon Epstein <simon.epstein@67bricks.com>
2023-10-17 16:15:11 +0100

    Added action to build and push image to ecr

--- push.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -e
-
-source .env
-
-docker push elifesciences/cermine:${CERMINE_VERSION}
-docker tag elifesciences/cermine:${CERMINE_VERSION} elifesciences/cermine:latest
-docker push elifesciences/cermine:latest
